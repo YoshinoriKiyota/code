c73e69d6ad13d5bff0f418df830f61c866a16e93
YoshinoriKiyota <157315602+YoshinoriKiyota@users.noreply.github.com>
2024-07-29 16:05:21 +0900

    Add files via upload

---
+++ portfolio-lp/page-recruit.php
@@ -0,0 +1,141 @@
+<?php get_header(); ?>
+
+<div id="contents" class="bg">
+
+<div id="main">
+
+<section>
+
+<h2>採用情報</h2>
+
+<table class="ta1">
+<tr>
+<th colspan="2" class="tamidashi">正社員募集</th>
+</tr>
+<tr>
+<th>見出し</th>
+<td><a href="#">ここに説明など入れ</a>て下さい。サンプルテキスト。</td>
+</tr>
+<tr>
+<th>見出し</th>
+<td>ここに説明など入れて下さい。サンプルテキスト。</td>
+</tr>
+<tr>
+<th>見出し</th>
+<td>ここに説明など入れて下さい。サンプルテキスト。</td>
+</tr>
+<tr>
+<th>見出し</th>
+<td>ここに説明など入れて下さい。サンプルテキスト。</td>
+</tr>
+<tr>
+<th>見出し</th>
+<td>ここに説明など入れて下さい。サンプルテキスト。</td>
+</tr>
+<tr>
+<th>見出し</th>
+<td>ここに説明など入れて下さい。サンプルテキスト。</td>
+</tr>
+</table>
+
+<table class="ta1">
+<tr>
+<th colspan="2" class="tamidashi">短期スタッフ募集</th>
+</tr>
+<tr>
+<th>見出し</th>
+<td>ここに説明など入れて下さい。サンプルテキスト。</td>
+</tr>
+<tr>
+<th>見出し</th>
+<td>ここに説明など入れて下さい。サンプルテキスト。</td>
+</tr>
+<tr>
+<th>見出し</th>
+<td>ここに説明など入れて下さい。サンプルテキスト。</td>
+</tr>
+<tr>
+<th>見出し</th>
+<td>ここに説明など入れて下さい。サンプルテキスト。</td>
+</tr>
+<tr>
+<th>見出し</th>
+<td>ここに説明など入れて下さい。サンプルテキスト。</td>
+</tr>
+<tr>
+<th>見出し</th>
+<td>ここに説明など入れて下さい。サンプルテキスト。</td>
+</tr>
+</table>
+
+</section>
+
+</div>
+<!--/main-->
+
+<div id="sub">
+
+<nav class="box">
+<h2>Menu</h2>
+<ul class="submenu">
+<li><a href="company.html">会社概要</a></li>
+<li><a href="link.html">リンク</a></li>
+<li><a href="#">メニュー</a></li>
+<li><a href="#">メニュー</a></li>
+<li><a href="#">メニュー</a></li>
+</ul>
+</nav>
+
+<section class="box">
+
+<h2>製品情報</h2>
+
+    <div class="list">
+    <a href="item.html">
+    <figure><img src="<?php echo get_theme_file_uri('/images/sample1.jpg'); ?>" alt="製品名"></figure>
+    <h4>製品情報</h4>
+    <p>簡単な説明をここに入れます。</p>
+    </a>
+    </div>
+    
+    <div class="list">
+    <a href="item.html">
+    <figure><img src="<?php echo get_theme_file_uri('/images/sample1.jpg'); ?>" alt="製品名"></figure>
+    <h4>製品情報</h4>
+    <p>簡単な説明をここに入れます。</p>
+    </a>
+    </div>
+
+    <div class="list">
+    <a href="item.html">
+    <figure><img src="<?php echo get_theme_file_uri('/images/sample1.jpg'); ?>" alt="製品名"></figure>
+    <h4>製品情報</h4>
+    <p>簡単な説明をここに入れます。</p>
+    </a>
+    </div>
+
+    <div class="list">
+    <a href="item.html">
+    <figure><img src="<?php echo get_theme_file_uri('/images/sample1.jpg'); ?>" alt="製品名"></figure>
+    <h4>製品情報</h4>
+    <p>簡単な説明をここに入れます。</p>
+    </a>
+    </div>
+
+</section>
+
+<section class="box">
+<h2>運営</h2>
+<p class="c"><strong>株式会社SAMPLE COMPANY</strong><br>
+<span class="mini1">東京都XX区XXXXビル3F<br>
+TEL:03-0000-0000<br>
+(AM9:00〜PM5:00 土日祝休み)</span></p>
+</section>
+
+</div>
+<!--/sub-->
+
+</div>
+<!--/contents-->
+
+<?php get_footer(); ?>
